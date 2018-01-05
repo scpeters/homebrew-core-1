5437f70dc8b377b4192f292b0c114a760b723eb3
Michka Popoff <michkapopoff@gmail.com>
2018-01-06 00:16:22 +0100

    swagger-codegen: Build a bottle for Linuxbrew

--- Formula/swagger-codegen.rb
+++ Formula/swagger-codegen.rb
@@ -1,3 +1,4 @@
+# swagger-codegen: Build a bottle for Linuxbrew
 class SwaggerCodegen < Formula
   desc "Generation of API clients, server stubs, documentation from OpenAPI/Swagger definition"
   homepage "https://swagger.io/swagger-codegen/"
