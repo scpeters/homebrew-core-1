7800d7b1eda8ad1668d1af0483c3c3d424309a8d
Michka Popoff <michkapopoff@gmail.com>
2018-01-29 19:21:47 +0100

    gv: Use xorg dep instead of X11 for Linuxbrew

--- Formula/gv.rb
+++ Formula/gv.rb
@@ -4,18 +4,19 @@ class Gv < Formula
   url "https://ftp.gnu.org/gnu/gv/gv-3.7.4.tar.gz"
   mirror "https://ftpmirror.gnu.org/gv/gv-3.7.4.tar.gz"
   sha256 "2162b3b3a95481d3855b3c4e28f974617eef67824523e56e20b56f12fe201a61"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "afc07271447c8455ac3313775832a1aa264245436b16f0333a4b7dddaf32d702" => :high_sierra
     sha256 "ae08a48cf7464fbf8af390ac017646cfa986fd95447ed8af5b91687350e10c93" => :sierra
     sha256 "80956fd9b222cb9185517f3bb2809d93fd23a8acff70e0bbb9e295cc3b54725b" => :el_capitan
     sha256 "aabf19a4073bae6b5b337c595169f18a3c4af9157bfcbd2b9b15744c3d0e4f2c" => :yosemite
-    sha256 "250e58f3c45e9c0d44d3bf671cfd0a004317dfe40cc2b5cfb7fb348df10c6d89" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "ghostscript" => "with-x11"
-  depends_on :x11
+  depends_on :x11 if OS.mac?
+  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
 
   skip_clean "share/gv/safe-gs-workdir"
 
