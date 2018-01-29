65165088fd764884084743151d000a9528187b0f
Michka Popoff <michkapopoff@gmail.com>
2018-01-29 21:34:59 +0100

    mupdf: Use xorg dep instead of X11 for Linuxbrew

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -4,6 +4,7 @@ class Mupdf < Formula
   url "https://mupdf.com/downloads/mupdf-1.12.0-source.tar.gz"
   sha256 "5c6353a82f1512f4f5280cf69a3725d1adac9c8b22377ec2a447c4fc45528755"
   head "https://git.ghostscript.com/mupdf.git"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -11,14 +12,14 @@ class Mupdf < Formula
     sha256 "4b9122fa576b3bb24754016fa9dad09113dcbe0fe982cff3341d0f8d718fae25" => :high_sierra
     sha256 "a9745c00b9efe835197f89e973f4b25698567c011aa1fdf3052c586ba3503a87" => :sierra
     sha256 "771078a7ae9a600933d1f38ff80389b7bc158e8aecde9c9c7bf7aecddc75e7a8" => :el_capitan
-    sha256 "041dc90a89a67305967e089552ac99b78aff093a53a2ed300bec5ee6a85f84ec" => :x86_64_linux
   end
 
-  depends_on :x11
+  depends_on :x11 if OS.mac?
   depends_on "openssl"
   unless OS.mac?
-    depends_on "linuxbrew/xorg/mesa"
     depends_on "linuxbrew/xorg/glu"
+    depends_on "linuxbrew/xorg/mesa"
+    depends_on "linuxbrew/xorg/xorg"
   end
 
   conflicts_with "mupdf-tools",
